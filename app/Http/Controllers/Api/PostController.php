1e880c4907b93f3dff2162c73c48d3f1efc91648
aldrianisaputra <aldrianisaputra04@gmail.com>
2022-08-18 11:23:32 +0700

    Menambahkan Data menggunakan API

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\SelfcareResource;
 use App\Models\SelfCare;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -16,5 +17,38 @@ class PostController extends Controller
 
         //return collection of posts as a resource
         return new SelfcareResource(true, 'List Data Catatan Perjalanan', $catper);
+    }
+    /**
+     * store
+     *
+     * @param  mixed $request
+     * @return void
+     */
+    public function store(Request $request)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'tanggal'     => 'required',
+            'waktu'     => 'required',
+            'lokasi'   => 'required',
+            'suhu'   => 'required',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+
+        //create catper
+        $catper = SelfCare::create([
+            'tanggal'     => $request->tanggal,
+            'waktu'     => $request->waktu,
+            'lokasi'   => $request->lokasi,
+            'suhu'   => $request->suhu,
+        ]);
+
+        //return response
+        return new SelfcareResource(true, 'Data Catatan Perjalanan Berhasil Ditambahkan!', $catper);
     }
 }
