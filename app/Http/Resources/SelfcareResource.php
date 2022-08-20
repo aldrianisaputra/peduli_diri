c72d2ce20e741c74e5ea22955e1ff043c4a55603
zivania02 <zivania04@gmail.com>
2022-08-20 19:10:15 +0700

    revisi1

--- app/Http/Resources/SelfcareResource.php
+++ app/Http/Resources/SelfcareResource.php
@@ -34,4 +34,33 @@ class SelfcareResource extends JsonResource
             'data'      => $this->resource
         ];
     }
+    public function update(Request $request, SelfCare $catper)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'tanggal'     => 'required',
+            'waktu'     => 'required',
+            'lokasi'   => 'required',
+            'suhu'   => 'required',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+
+            //update post with new image
+            $catper->update([
+                'tanggal'     => $request->tanggal,
+                'waktu'     => $request->waktu,
+                'lokasi'   => $request->lokasi,
+                'suhu'   => $request->suhu,
+            ]);
+
+        
+
+        //return response
+        return new SelfcareResource(true, 'Data catper Berhasil Diubah!', $catper);
+    }
 }
